0ee9d169996ec2697fe5317ebf42a71c60467f7a
Igor Maliy <imaliy.filsh@gmail.com>
2020-05-19 11:46:42 +0300

    Development

--- views/layout/_footer.php
+++ views/layout/_footer.php
@@ -6,7 +6,7 @@ use Mailery\Icon\Icon;
 ?>
 <footer class="ml-sm-auto col-lg-12 pt-3 pb-3 px-4 footer-sticky">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?= date('Y'); ?> <a href="#" target="_blank" class="text-muted">Brand Label</a>. All rights reserved.</span>
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?= date('Y'); ?> <a href="#" target="_blank" class="text-muted">Mailery Platform</a>. All rights reserved.</span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-muted text-center">Hand-crafted &amp; made with <?= Icon::widget()->name('heart-outline')->options(['class' => 'text-primary']); ?></span>
     </div>
 </footer>
