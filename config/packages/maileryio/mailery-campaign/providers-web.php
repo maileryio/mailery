3fbd3c629fe417033cc7c3dfb640362ccb4722b5
Igor Maliy <imaliy.filsh@gmail.com>
2021-08-22 20:33:23 +0000

    Development

--- config/packages/maileryio/mailery-campaign/providers-web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Mailery\Campaign\Provider\RouteCollectorServiceProvider;
-
-return [
-    RouteCollectorServiceProvider::class => RouteCollectorServiceProvider::class,
-];
