3b6e08240640530a29af7fbb0533d1b64bec4674
Igor Maliy <imaliy.filsh@gmail.com>
2022-03-09 00:23:32 +0200

    Development

--- views/layout/_footer.php
+++ views/layout/_footer.php
@@ -1,7 +1,5 @@
 <?php declare(strict_types=1);
 
-use Mailery\Icon\Icon;
-
 /** @var Mailery\Web\View\WebView $this */
 /** @var \Mailery\Brand\Service\BrandLocatorInterface $brandLocator */
 $cssClass = 'ml-sm-auto col-lg-12 pb-3 px-4 footer-sticky';
@@ -13,6 +11,6 @@ if ($brandLocator->hasBrand()) {
     <hr />
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?= date('Y'); ?> <a href="#" target="_blank" class="text-muted">Mailery Platform</a>. All rights reserved.</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-muted text-center">Hand-crafted &amp; made with <?= Icon::widget()->name('heart-outline')->options(['class' => 'text-primary']); ?></span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-muted text-center">Hand-crafted &amp; made in Ukraine &#127482;&#127462;</span>
     </div>
 </footer>
