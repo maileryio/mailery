894d2f350c8116937c52dff1ebd57edcde08be87
Igor Maliy <imaliy.filsh@gmail.com>
2022-05-13 23:58:06 +0300

    Development

--- config/bootstrap-web.php
+++ config/bootstrap-web.php
@@ -2,4 +2,12 @@
 
 declare(strict_types=1);
 
-return [];
+return [
+    static function () use($params) {
+        // sometimes debug throw fatal error "Allowed memory size of ..."
+        $debugEnabled = $params['yiisoft/yii-debug']['enabled'] ?? false;
+        if ($debugEnabled === true) {
+            ini_set('memory_limit', '1024M');
+        }
+    }
+];
